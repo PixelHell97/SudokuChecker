4ee9319cd3a0e27e6955c215bfe4a9b8d04125e2
Abdallah Bakhet <abdallahM_bakhet@outlook.com>
2025-04-02 19:22:49 +0200

    add basic test cases

--- src/main/kotlin/Test.kt
+++ src/main/kotlin/Test.kt
@@ -1,7 +1,53 @@
 package org.example
 
 fun main() {
+    test(
+        name = "When given an empty sudoku board it should return true",
+        result = isValidSudokuBoard(""),
+        correctAnswer = true,
+    )
 
+    test(
+        name = "When given a valid sudoku board it should return true",
+        result = isValidSudokuBoard(""),
+        correctAnswer = true,
+    )
+
+    test(
+        name = """
+            When given a sudoku board that contains 
+            anything instead of number or (-) in one of 
+            it's houses it should return false
+            """.trimIndent(),
+        result = isValidSudokuBoard(""),
+        correctAnswer = false,
+    )
+
+    test(
+        name = """
+            When given a sudoku board that contains 
+            a duplicated number in the same row or column 
+            it should return false
+            """.trimIndent(),
+        result = isValidSudokuBoard(""),
+        correctAnswer = false,
+    )
+
+    test(
+        name = "When given a sudoku board that contains a duplicated number in the same box it should return false",
+        result = isValidSudokuBoard(""),
+        correctAnswer = false,
+    )
+
+    test(
+        name = """
+            When given a sudoku board that contains 
+            a number smaller than 1 and bigger than 9 in one of it's houses 
+            it should return false
+            """.trimMargin(),
+        result = isValidSudokuBoard(""),
+        correctAnswer = false,
+    )
 }
 
 fun test(
